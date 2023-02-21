b95270db327dae67e700d0057a5bd1e32349f842
Jacob Bradley <cobrbradley@gmail.com>
2023-02-21 01:53:06 +0000

    docs: adding new comment instructions for generating tables

--- code/get_tables.R
+++ code/get_tables.R
@@ -3,7 +3,9 @@ library(here)
 
 get_tables <- function(results) {
   tables <- list()
+  
   # tables$table_1 <- ...
+  # add code to generate new tables here
   
   for (table_index in seq_len(length(tables))) {
     write_csv(tables[[table_index]], 
