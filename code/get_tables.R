8b574c33e303b39325bec8d87bdb63d2cca820bf
Jacob Bradley <cobrbradley@gmail.com>
2023-02-21 02:12:39 +0000

    feat: demonstrate table creation

--- code/get_tables.R
+++ code/get_tables.R
@@ -1,15 +1,18 @@
 library(tidyverse)
 library(here)
+library(xtable)
 
 get_tables <- function(results) {
   tables <- list()
+  
+  tables$mtcars_full <- results$mt_table
   
   # tables$table_1 <- ...
   # add code to generate new tables here
   
   for (table_index in seq_len(length(tables))) {
-    write_csv(tables[[table_index]], 
-              paste0(here("tables/", names(tables)[table_index])))
+    print(xtable(tables[[table_index]]), 
+          file = paste0(here("tables/", names(tables)[table_index])))
   }
   return(tables)
 }
