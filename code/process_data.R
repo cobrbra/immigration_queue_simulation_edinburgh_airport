d9c3aab890c978c6f52eef09645c23db39d8ecf4
Jacob Bradley <cobrbradley@gmail.com>
2023-02-22 17:15:14 +0000

    feat: revert to old data processing in example

--- code/process_data.R
+++ code/process_data.R
@@ -4,8 +4,8 @@ library(here)
 get_processed_data <- function(file) {
   
   
-  processed_data <- read_csv(file) #%>% 
-    # filter(mpg < 20)
+  processed_data <- read_csv(file) %>%
+    filter(mpg < 20)
   write_csv(processed_data, here("processed_data/example_processed_data.csv"))
   
   
