f7567366accf9a0bd6fb1f251f69ea0520a5159e
Isabella Deutsch <s1989890@ed.ac.uk>
2023-02-22 15:18:52 +0000

    feat: iris data figure

--- code/bella_rough.R
+++ code/bella_rough.R
@@ -1,1 +1,11 @@
 print("Hi, I'm Bella")
+
+library("ggplot2")
+library("here")
+
+plot1 <- ggplot(aes(x = Sepal.Length, y = Sepal.Width), 
+                data = iris) +
+  geom_point()
+plot1
+
+ggsave(here("figures/plot_1_b.png"))
