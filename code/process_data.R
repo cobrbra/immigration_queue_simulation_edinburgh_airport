0fcede8b0007a211022308e0385ffb34aedfa731
Jacob Bradley <cobrbradley@gmail.com>
2023-02-22 16:39:12 +0000

    feat: change data pre-processing for example workflow

--- code/process_data.R
+++ code/process_data.R
@@ -2,8 +2,12 @@ library(tidyverse)
 library(here)
 
 get_processed_data <- function(file) {
-  processed_data <- read_csv(file) %>% 
-    filter(mpg < 20)
+  
+  
+  processed_data <- read_csv(file) #%>% 
+    # filter(mpg < 20)
   write_csv(processed_data, here("processed_data/example_processed_data.csv"))
+  
+  
   return(processed_data)
 }
