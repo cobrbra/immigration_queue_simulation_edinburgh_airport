5aa79f56edc7a18a33aa2999315d7ed29af8458d
Jacob Bradley <cobrbradley@gmail.com>
2023-02-22 14:59:00 +0000

    feat: create example figure

--- code/jacob_rough.R
+++ code/jacob_rough.R
@@ -8,3 +8,10 @@ mtcars %>%
 mtcars %>% 
   filter(mpg < 20) %>% 
   dim()
+
+fig_1 <- data.frame(x = 1:10, y = 15:24) %>% 
+  ggplot(aes(x = x, y = y)) + geom_point()
+
+fig_1
+
+ggsave(here("figures/fig_1.png"), fig_1)
