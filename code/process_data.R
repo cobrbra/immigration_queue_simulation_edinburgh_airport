733d5bc50269d9880b26d09f95bea76b844cac44
Jacob Bradley <cobrbradley@gmail.com>
2023-02-24 01:52:02 +0000

    feat: add planes dataset

--- code/process_data.R
+++ code/process_data.R
@@ -11,3 +11,16 @@ get_processed_data <- function(file) {
   
   return(processed_data)
 }
+
+
+process_planes_data <- function(file) {
+  planes_cols <- c("icao_code", "iata_code", "airport_name", "city", "country",
+                   "lat_degrees", "lat_minutes", "lat_seconds", "lat_direction",
+                   "lon_degrees", "lon_minutes", "lon_seconds", "lon_direction",
+                   "altitude", "lat_dec", "lon_dec")
+  processed_data <- read_delim(file, # read_delim(here("raw_data/planes/GlobalAirportDatabase.txt"), # 
+                               delim = ":",
+                               col_names = planes_cols) 
+  write_csv(processed_data, here("processed_data/planes_data.csv"))
+
+}
