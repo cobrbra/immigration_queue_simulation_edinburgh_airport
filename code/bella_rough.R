e9340d1332069f5edfe42d867fd505facc810ee1
Isabella Deutsch <s1989890@ed.ac.uk>
2023-03-03 13:45:26 +0000

    feat: simulate data sets to test function

--- code/bella_rough.R
+++ code/bella_rough.R
@@ -25,79 +25,39 @@ set.seed(4)
 N_passenger <- 1000
 lambda_passenger <- 5
 
-passengers <- data.frame(Passenger.ID = paste0("C", str_pad(1:N_passenger, 6, pad = "0")),
-                        Time.Arrival = cumsum(rexp(n = N_passenger, rate = lambda_passenger)),
-                        Time.Desk.Start = numeric(N_passenger),
-                        Time.Desk.End = numeric(N_passenger),
-                        Desk.Handled = character(N_passenger) )
+passengers <- data.frame(passenger_id = paste0("P", str_pad(1:N_passenger, 6, pad = "0")),
+                         aircraft_id = character(N_passenger),
+                         nationality = character(N_passenger),
+                         aircraft_arrival = numeric(N_passenger), 
+                         arrival_at_hall = numeric(N_passenger),
+                         arrival_at_queue = cumsum(rexp(n = N_passenger, rate = lambda_passenger)),
+                         arrival_from_immigration = numeric(N_passenger),
+                         queue_length = numeric(N_passenger),
+                         e_gate = boolean(N_passenger)) 
 
 
-# Desks
+
+
+# Checks
 
 N_desk <- 5
 mu_desk <- pmax(0.5, rnorm(N_desk, mean = 1, sd = 0.2))
-Desk.ID <- paste0("D", str_pad(1:N_desk, 2, pad = "0"))
+Check.ID <- paste0("D", str_pad(1:N_desk, 2, pad = "0"))
 
 desks <- list(N_desk = N_desk, 
               mu_desk = mu_desk,
-              Desk.ID = Desk.ID)
+              Check.ID = Check.ID)
 
-
-# Function
-
-
-queue <- function(passengers, desks){
-  
-  # Initialise variables
-  
-  # desks
-  N_desk <- desks$N_desk 
-  mu_desk <- desks$mu_desk
-  Desk.ID <- desks$Desk.ID
-  desk_times <- rep(0, times = N_desk)
-  
-  # passengers
-  N_passenger <- dim(passengers)[1]
-  
-  
-  for(i in 1:N_passenger){ #loop over all passengers
-    
-    next_arrival_time <- passengers$Time.Arrival[i]
-    
-    # if a desk is idle, update their tie
-    desk_times <- pmax(desk_times, next_arrival_time)
-    
-    #decide which desk to send the passenger to
-    next_free_desk <- which(desk_times == min(desk_times))[1] # works even when some desks have the same time, edit for E-Gates with conditions
-    next_free_desk_time <- min(desk_times)
-    
-    # how long it takes to handle passenger
-    handling_time <- rexp(1, mu_desk[next_free_desk])
-    
-    # update accordingly for passenger
-    passengers$Time.Desk.Start[i] <- next_free_desk_time
-    passengers$Time.Desk.End[i] <- next_free_desk_time + handling_time
-    passengers$Desk.Handled[i] <- desks$Desk.ID[next_free_desk]
-    
-    # update desk 
-    desk_times[next_free_desk] <- desk_times[next_free_desk] + handling_time
-    
-    
-  }
-  
-  return(passengers)
-  
-}
 
 
 
 passengers <- queue(passengers = passengers, desks = desks)
 
 
-wait_times <- passengers$Time.Desk.Start - passengers$Time.Arrival
+wait_times <- passengers$Time.Check.Start - passengers$Time.Arrival
 
 plot(wait_times)
 plot(density(wait_times))
 summary(wait_times)
 
-tapply(X = wait_times, INDEX = passengers$Desk.Handled, FUN = summary)
+tapply(X = wait_times, INDEX = passengers$Check.Handled, FUN = summary)
