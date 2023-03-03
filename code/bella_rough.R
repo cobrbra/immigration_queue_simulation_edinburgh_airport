b559f3be478cf6536f9665c5944c44da7c8f5676
Isabella Deutsch <s1989890@ed.ac.uk>
2023-03-03 14:54:33 +0000

    feat: added functions to simulate airplane and passengers from it

--- code/bella_rough.R
+++ code/bella_rough.R
@@ -2,25 +2,81 @@ print("Hi, I'm Bella")
 
 library("ggplot2")
 library("here")
-
-plot1 <- ggplot(aes(x = Sepal.Length, y = Sepal.Width), 
-                data = iris) +
-  geom_point()
-plot1
-
-ggsave(here("figures/plot_1_b.png"))
-
-
-
-### try out queuing (from arriving at the front of the queue) ###
-# 4th process # 
-
 library("stringr")
 
+
+
+###########################
+####### 2nd Process ####### 
+########## Route ##########
+###########################
+
 set.seed(4)
+walktimes <- data.frame(gate = 1:23, waltime_min = rpois(23, 6))
+
+n_aircrafts <- 5
+
+aircrafts <- data.frame(
+  aircraft_id = paste0("A", str_pad(1:n_aircrafts, 3, pad = "0")),
+  aircraft_scheduled = numeric(n_aircrafts),
+  aircraft_arrival = cumsum(rexp(n = n_aircrafts, rate = 0.2)), 
+  airline = rep("KLM", times = n_aircrafts),
+  type = rep("Boing 737-800", times = n_aircrafts),
+  capacity = rep(160, times = n_aircrafts),
+  passenger_on_board = 160 - rpois(n = n_aircrafts, lambda = 10),
+  coached = sample(c(TRUE, FALSE), size = n_aircrafts, replace = TRUE)
+)
 
 
-# passengers 
+
+get_passenger_from_aircrafts <- function(aircrafts, seed = NULL){
+  
+  if (!is.null(seed)) {set.seed(seed)}
+  
+  n_passengers_aircraft <- aircrafts$passenger_on_board
+  n_passengers <- sum(n_passengers_aircraft)
+  
+  passengers <- data.frame(
+    
+    passenger_id =paste0("P", str_pad(1:n_passengers, 6, pad = "0")),
+    aircraft_id = rep(aircrafts$aircraft_id, n_passengers_aircraft),
+    nationality = character(n_passengers),
+    aircraft_arrival = rep(aircrafts$aircraft_arrival, n_passengers_aircraft),
+    coached = rep(aircrafts$coached, n_passengers_aircraft),
+    arrival_at_hall = character(n_passengers)
+    
+  )
+  
+  
+  return(passengers)
+}
+
+
+
+passengers <- get_passenger_from_aircrafts(aircrafts)
+
+check_passengers_from_route(passengers)
+
+
+route_process  <- function(passengers, seed = NULL){
+  
+  if (!is.null(seed)) {set.seed(seed)}
+  
+  
+  
+}
+
+
+
+###########################
+####### 4th process ####### 
+####### Immigration ####### 
+###########################
+
+
+# simulate the data sets (for trials)
+
+set.seed(4)
 
 N_passenger <- 1000
 lambda_passenger <- 5
@@ -33,31 +89,32 @@ passengers <- data.frame(passenger_id = paste0("P", str_pad(1:N_passenger, 6, pa
                          arrival_at_queue = cumsum(rexp(n = N_passenger, rate = lambda_passenger)),
                          arrival_from_immigration = numeric(N_passenger),
                          queue_length = numeric(N_passenger),
-                         e_gate = boolean(N_passenger)) 
+                         e_gate = character(N_passenger)) 
+
+
+n_checks <- 6
+handling_rates <- pmax(0.5, rnorm(n_checks, mean = 1, sd = 0.2))
+check_ids <- paste0("CD", str_pad(1:n_checks, 2, pad = "0"))
+
+checks <- list(n_checks = n_checks, 
+              handling_rates = handling_rates,
+              check_ids = check_ids)
 
 
 
-
-# Checks
-
-N_desk <- 5
-mu_desk <- pmax(0.5, rnorm(N_desk, mean = 1, sd = 0.2))
-Check.ID <- paste0("D", str_pad(1:N_desk, 2, pad = "0"))
-
-desks <- list(N_desk = N_desk, 
-              mu_desk = mu_desk,
-              Check.ID = Check.ID)
+# check if passengers passes
+check_passengers_from_immigration(passengers)
 
 
+# get passengers checked
+passengers <- immigration_queue(passengers = passengers, checks = checks, 
+                                handling_time_func = get_check_handling_time, seed = 4)
 
-
-passengers <- queue(passengers = passengers, desks = desks)
-
-
-wait_times <- passengers$Time.Check.Start - passengers$Time.Arrival
+# analysis
+wait_times <- passengers$check_end_time - passengers$arrival_at_queue
 
 plot(wait_times)
 plot(density(wait_times))
 summary(wait_times)
 
-tapply(X = wait_times, INDEX = passengers$Check.Handled, FUN = summary)
+tapply(X = wait_times, INDEX = passengers$check_handled, FUN = summary)
