73883b7a01b12930c9b791e7c84edebb79cb27d3
Isabella Deutsch <s1989890@ed.ac.uk>
2023-03-03 16:10:24 +0000

    feat: function for step 2 (route), which takes passengers from airplane to the back of the immigration hall

--- code/step_2_route.R
+++ code/step_2_route.R
@@ -7,7 +7,9 @@ check_passengers_from_route <- function(passengers_from_route) {
     "aircraft_id",
     "nationality",
     "aircraft_arrival",
-    "arrival_at_hall"
+    "arrival_at_hall",
+    "walk_time", 
+    "taxi_time"
   )
   if (any(!(necessary_columns %in% colnames(passengers_from_route)))) {
     stop(
@@ -19,6 +21,59 @@ check_passengers_from_route <- function(passengers_from_route) {
     stop("Passenger IDs are not unique.")
   }
 }
+
+
+
+
+
+
+route_process  <- function(passengers, seed = NULL){
+  
+  if (!is.null(seed)) {set.seed(seed)}
+  
+  aircraft_id <- unique(passengers$aircraft_id)
+  
+  n_aircrafts <- length(aircraft_id)
+  
+  n_passengers_aircraft <- table(passengers$aircraft_id)
+  rownames(n_passengers_aircraft) <- NULL
+  
+  coached <- as.logical(tapply(X = passengers$coached, FUN = prod, INDEX = passengers$aircraft_id))
+  
+  
+  # get the minimum arrival at hall per aircraft
+  passengers$arrival_at_hall <- passengers$aircraft_arrival + passengers$taxi_time + passengers$walk_time
+  
+  for(j in seq_len(n_aircrafts)){
+    
+    # spread out passenger arrival differently for coached and contact
+    
+    bool_iter <- passengers$aircraft_id == aircraft_id[j]
+    
+    if(coached[j]){
+      
+      # we should get this distribution from them 
+      passengers$arrival_at_hall[bool_iter] <- passengers$arrival_at_hall[bool_iter] + runif(n = sum(bool_iter), min = 0.5, max = 4)
+      
+    } else {
+      
+      # we should get this distribution from them 
+      passengers$arrival_at_hall[bool_iter] <- passengers$arrival_at_hall[bool_iter] + cumsum(rexp(n = sum(bool_iter), rate = 12))
+    }
+    
+    
+  }
+  
+  passengers <- passengers[order(passengers$arrival_at_hall), ]
+  
+  return(passengers)
+  
+  
+}
+
+
+
+
 
 get_passengers_from_route <- function(passengers_from_aircraft) {
   passengers_from_route <- data.frame(
