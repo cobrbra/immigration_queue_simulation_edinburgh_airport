f46452b1f4061a38505ba2d0eeb3869bcd826e8e
Jacob Bradley <cobrbradley@gmail.com>
2023-03-16 10:42:20 +0000

    refactor: neaten step 2 functions

--- code/step_2_route.R
+++ code/step_2_route.R
@@ -4,12 +4,10 @@ check_passengers_from_route <- function(passengers_from_route) {
   }
   necessary_columns <- c(
     "passenger_id",
-    "aircraft_id",
+    "flight_id",
     "nationality",
     "aircraft_arrival",
-    "arrival_at_hall",
-    "walk_time", 
-    "taxi_time"
+    "arrival_at_hall"
   )
   if (any(!(necessary_columns %in% colnames(passengers_from_route)))) {
     stop(
@@ -23,69 +21,80 @@ check_passengers_from_route <- function(passengers_from_route) {
 }
 
 
-
-
-
-
-route_process  <- function(passengers, seed = NULL){
-  
+get_coach_time <- function(passengers, seed = NULL) {
   if (!is.null(seed)) {set.seed(seed)}
-  
-  aircraft_id <- unique(passengers$aircraft_id)
-  
-  n_aircrafts <- length(aircraft_id)
-  
-  n_passengers_aircraft <- table(passengers$aircraft_id)
-  rownames(n_passengers_aircraft) <- NULL
-  
-  coached <- as.logical(tapply(X = passengers$coached, FUN = prod, INDEX = passengers$aircraft_id))
-  
-  
-  # get the minimum arrival at hall per aircraft
-  passengers$arrival_at_hall <- passengers$aircraft_arrival + passengers$taxi_time + passengers$walk_time
-  
-  for(j in seq_len(n_aircrafts)){
-    
-    # spread out passenger arrival differently for coached and contact
-    
-    bool_iter <- passengers$aircraft_id == aircraft_id[j]
-    
-    # TODO: make these calls to randomness separate function calls
-    if(coached[j]){
-      
-      # we should get this distribution from them 
-      passengers$arrival_at_hall[bool_iter] <- passengers$arrival_at_hall[bool_iter] + runif(n = sum(bool_iter), min = 0.5, max = 4)
-      
-    } else {
-      
-      # we should get this distribution from them 
-      passengers$arrival_at_hall[bool_iter] <- passengers$arrival_at_hall[bool_iter] + cumsum(rexp(n = sum(bool_iter), rate = 12))
-    }
-    
-    
-  }
-  
-  passengers <- passengers[order(passengers$arrival_at_hall), ]
-  
-  return(passengers)
-  
-  
+  n_flights <- n_distinct(passengers$flight_id)
+  passengers_with_coach_time <- passengers %>% 
+    nest(cols = -c(coached, flight_id)) %>% 
+    mutate(coach_time = if_else(coached, pmax(60, rnorm(n_flights, 23*60, 6*60)), 0)) %>% 
+    unnest(cols)
+  return(passengers_with_coach_time)
 }
 
-
-
+get_walk_time <- function(coached, n_passengers, seed = NULL) {
+  if (!is.null(seed)) {set.seed(seed)}
+  n_passengers <- length(coached)
+  coached_walk_time <- cumsum(rexp(n_passengers, .5))
+  contact_walk_time <- cumsum(rexp(n_passengers, .2)) # TODO: this cumsum is over the whole dataste and shouldn't be!
+  walk_time <- if_else(coached, coached_walk_time, contact_walk_time)
+  return(walk_time)
+}
 
 
 get_passengers_from_route <- function(passengers_from_aircraft) {
-  passengers_from_route <- data.frame(
-    passenger_id = character(),
-    aircraft_id = character(),
-    nationality = character(),
-    aircraft_arrival = character(),
-    arrival_at_hall = character(),
-    walk_time = vector(),
-    taxi_time = vector()
-  )
+  passengers_from_route <- passengers_from_aircraft %>% 
+    get_coach_time() %>% 
+    mutate(walk_time = get_walk_time(coached = coached)) %>% 
+    mutate(arrival_at_hall = aircraft_arrival + coach_time + walk_time) %>% 
+    # select(-c(coach_time, walk_time)) %>% 
+    arrange(arrival_at_hall)
+  
   check_passengers_from_route(passengers_from_route)
   return(passengers_from_route)
 }
+
+
+# route_process  <- function(passengers, seed = NULL){
+#   
+#   if (!is.null(seed)) {set.seed(seed)}
+#   
+#   flight_id <- unique(passengers$flight_id)
+#   
+#   n_aircrafts <- length(flight_id)
+#   
+#   n_passengers_aircraft <- table(passengers$flight_id)
+#   rownames(n_passengers_aircraft) <- NULL
+#   
+#   coached <- as.logical(tapply(X = passengers$coached, FUN = prod, INDEX = passengers$flight_id))
+#   
+#   
+#   # get the minimum arrival at hall per aircraft
+#   passengers$arrival_at_hall <- passengers$aircraft_arrival + passengers$taxi_time + passengers$walk_time
+#   
+#   for(j in seq_len(n_aircrafts)){
+#     
+#     # spread out passenger arrival differently for coached and contact
+#     
+#     bool_iter <- passengers$flight_id == flight_id[j]
+#     
+#     # TODO: make these calls to randomness separate function calls
+#     if(coached[j]){
+#       
+#       # we should get this distribution from them 
+#       passengers$arrival_at_hall[bool_iter] <- passengers$arrival_at_hall[bool_iter] + runif(n = sum(bool_iter), min = 0.5, max = 4)
+#       
+#     } else {
+#       
+#       # we should get this distribution from them 
+#       passengers$arrival_at_hall[bool_iter] <- passengers$arrival_at_hall[bool_iter] + cumsum(rexp(n = sum(bool_iter), rate = 12))
+#     }
+#     
+#     
+#   }
+#   
+#   passengers <- passengers[order(passengers$arrival_at_hall), ]
+#   
+#   return(passengers)
+#   
+#   
+# }
