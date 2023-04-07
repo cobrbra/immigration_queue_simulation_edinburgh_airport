60e467c2a07f5c59273364a2358f490f1b9b7194
Jacob Bradley <cobrbradley@gmail.com>
2023-04-07 20:07:16 +0100

    feat: demo full runthrough post-dividing into weeks

--- code/jacob_rough.R
+++ code/jacob_rough.R
@@ -3,22 +3,6 @@ library(here)
 library(scales)
 library(readxl)
 
-ex_week_2023_aircrafts <- tar_read(future_aircrafts_arrivals) %>% 
-  filter(format(sched_aircraft_datetime_posix, format = "%Y") == 2023) %>%
-  mutate(aircraft_datetime_int = simulate_delay_times(flight_id, .21, .58, .21, 50*60, 21*60) + sched_aircraft_datetime_int) %>% 
-  get_datetime_alternates(column_prefixes = c("aircraft", "sched_aircraft")) %>% 
-  mutate(coached = get_coached_status(flight_id)) %>% 
-  simulate_future_airport_classification(tar_read(n_passenger_quantiles), seed = 1234)
-
-ex_week_2023_passengers <- get_passengers_after_aircrafts(ex_week_2023_aircrafts,
-                                                      tar_read(EU_plus_hubs), tar_read(other_hubs),
-                                                      tar_read(prop_nationality), tar_read(UK_plus_countries),
-                                                      tar_read(EU_plus_countries), tar_read(load_factor_mean), 
-                                                      tar_read(load_factor_sd), seed = 1234)
-
-
-ex_week_2023_routes <- get_passengers_after_route(ex_week_2023_passengers, seed = 1234)
-
 n_desks <- 9
 # desk_rates <- pmax(1/200, rnorm(n_desks, mean = 1/60, sd = 0.01))
 desk_rates <- pmax(40, rnorm(n_desks, mean = 45, sd = 10))
@@ -37,41 +21,68 @@ bordercheck_egates <- list(n_borderchecks = n_egates,
                            bordercheck_rates = eGate_rates,
                            bordercheck_ids = eGate_ids)
 
-
-ex_week_2023_immigration <- immigration_queue(ex_week_2023_routes, bordercheck_desks = bordercheck_desks, 
+seed <- 1234
+simulate_future <- tar_read(future_aircrafts_arrivals) %>% 
+  mutate(aircraft_datetime_int = simulate_delay_times(flight_id, .21, .58, .21, 50*60, 21*60, seed = seed) + sched_aircraft_datetime_int) %>% 
+  get_datetime_alternates(column_prefixes = c("aircraft")) %>% 
+  mutate(coached = get_coached_status(flight_id, seed = seed)) %>% 
+  simulate_future_airport_classification(tar_read(n_passenger_quantiles), seed = seed) %>% 
+  get_passengers_after_aircrafts(tar_read(EU_plus_hubs), tar_read(other_hubs),
+                                 tar_read(prop_nationality), tar_read(UK_plus_countries),
+                                 tar_read(EU_plus_countries), tar_read(load_factor_mean), 
+                                 tar_read(load_factor_sd), seed = seed) %>% 
+  get_passengers_after_route(seed = seed) %>% 
+  group_by(route_date_posix) %>% 
+  nest() %>% 
+  mutate(queue_data = map(data, 
+                          ~ immigration_queue(., bordercheck_desks = bordercheck_desks, 
                                               bordercheck_egates = bordercheck_egates, egate_uptake_prop = 0.8, 
-                                              egate_failure_prop = 0.05, egate_failed_passenger_next = 0.75, seed = 1234)
+                                              egate_failure_prop = 0.05, egate_failed_passenger_next = 0.75, seed = seed))) %>% 
+  mutate(prop_failed_queue_time = unlist(map(queue_data, ~ mean((.$bordercheck_start_time - .$route_datetime_int) >= 15*60)))) %>% 
+  select(-data) %>% 
+  unnest(queue_data)
 
-input_times <- seq(from = as.numeric(as.POSIXct("2023-07-10 00:00:00")) , to =  as.numeric(as.POSIXct("2023-07-17 00:00:00")), by = 900)
-ql <- get_queue_length(passengers = ex_week_2023_immigration, input_times = input_times) %>% 
+simulate_future %>% 
+  mutate(wait_time = bordercheck_start_time - route_datetime_int,
+         Year = format(route_date_posix, format = "%Y")) %>% 
+  group_by(Year, egate_used) %>% 
+  nest() %>% 
+  mutate(prop_failing_sla = unlist(map(data, ~ mean(.$wait_time >= 15*60)))) %>% 
+  ggplot(aes(x = Year, y = prop_failing_sla, fill = egate_used)) + 
+    geom_col(position = "dodge") + 
+    scale_fill_manual(values = edi_airport_colours) +
+    # theme_edi_airport() +
+    labs(y = "Proportion of passengers with wait times breaching SLA")
+  
+
+input_times <- seq(from = as.numeric(as.POSIXct("2023-07-10 00:00:00")) , to =  as.numeric(as.POSIXct("2023-07-11 00:00:00")), by = 900)
+queue_lengths <- get_queue_length(passengers = ex_day_2023_immigration, input_times = input_times) %>% 
   mutate(input_datetime_int = input_times) %>% 
   get_datetime_alternates(column_prefixes = c("input"))
 
-ql %>% 
-  pivot_longer(cols = c(desk, egate), names_to = "check_type", values_to = "queue_length") %>%  
-  ggplot(aes(x = input_datetime_posix, y = queue_length, colour = check_type)) + geom_point()
   
-rel_scales <- 5
-rough_arrivals_queue_comp <- ggplot() + 
-  geom_histogram(
-    data = ex_week_2023_passengers,
-    mapping = aes(x = aircraft_datetime_posix),
-    alpha = 0.5,
-    fill = edi_airport_colours[3]
-  ) + 
-  geom_point(
-    data = ql %>% 
-      pivot_longer(cols = c(desk, egate), names_to = "check_type", values_to = "queue_length"),
-    mapping = aes(x = input_datetime_posix, y = rel_scales*queue_length, colour = check_type)
-  ) + 
-  scale_y_continuous(
-      name = "Queue Length",
-      sec.axis = sec_axis( trans=~./rel_scales, name="Passengers arriving")
-  ) +
+ex_day_2023_passengers %>% 
+  mutate(coached = factor(if_else(coached, "Coached", "Contact"),
+                          levels = c("Contact", "Coached"))) %>% 
+  ggplot() + 
+    geom_histogram(
+      mapping = aes(x = aircraft_datetime_posix, fill = coached),
+      position = position_stack(reverse = TRUE)
+    ) + 
+    labs(x = "Date and Time") + 
+    # theme_edi_airport()0 +
+    theme(legend.title = element_blank(),
+          legend.position = "top") +
+    scale_fill_manual(values = edi_airport_colours) 
+
+queue_lengths %>% 
+  pivot_longer(cols = c(desk, egate), names_to = "Check Type", values_to = "Queue Length") %>%   
+  ggplot(aes(x = input_datetime_posix, y = `Queue Length`, colour = `Check Type`)) + 
+  geom_point() + 
+  geom_hline(yintercept = 650, linetype = 2, colour = "red") +
+  scale_colour_manual(values = edi_airport_colours) + 
   labs(x = "Date and Time") + 
-  theme_edi_airport() +
-  theme(legend.title = element_blank()) +
-  scale_colour_manual(values = edi_airport_colours)
+  theme(legend.position = "bottom")
 ggsave(filename = here("figures/rough_arrivals_queue_comp.png"), rough_arrivals_queue_comp, width = 7, height = 7)
 
 ex_week_2023_passengers %>% 
