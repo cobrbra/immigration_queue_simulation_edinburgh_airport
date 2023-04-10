a4c5fbdfa4af8fce13acb3aefcc4cdf80a0fdc7e
Jacob Bradley <cobrbradley@gmail.com>
2023-04-10 16:37:48 +0100

    feat: automatically generate table .tex

--- code/get_tables.R
+++ code/get_tables.R
@@ -2,14 +2,26 @@ library(tidyverse)
 library(here)
 library(xtable)
 
-get_tables <- function(results) {
+get_tables <- function(future_aircrafts_arrivals, ...) {
   # results <- targets::tar_read(example_results) # Use for debugging, COMMENT WHEN RUNNING TARGETS
   
   tables <- list()
   captions <- list()
   
-  tables$mtcars_full <- results$mt_table # read_csv("processed_data/example_processed_data.csv") # 
-  captions$mtcars_full <- "MTCars dataset restricted to observations with $<20$ miles per gallon"
+  tables$anticipated_schedule <- future_aircrafts_arrivals %>% 
+    mutate(Year = format(sched_aircraft_datetime_posix, "%Y")) %>% 
+    group_by(Year) %>% 
+    nest() %>% 
+    mutate(
+      `Number of Flights` = unlist(map(data, nrow)),
+      `Start Date` = unlist(map(data, 
+                                ~ format(min(.$sched_aircraft_date_posix),
+                                         format = "%d/%m"))),
+      `End Date` = unlist(map(data,
+                             ~ format(max(.$sched_aircraft_date_posix),
+                                      format = "%d/%m")))) %>% 
+    select(Year, `Start Date`, `End Date`, `Number of Flights`) 
+  captions$anticipated_schedule <- "Anticipated flight schedule, 2023-2027."
   
   # tables$table_1 <- ...
   # add code to generate new tables here
@@ -26,7 +38,8 @@ get_tables <- function(results) {
     print(captions[[table_name]])
     print(xtable(tables[[table_index]],
                  caption = captions[[table_name]]), 
-          file = paste0(here("tables/"), table_name, ".tex"))
+          file = paste0(here("tables/"), table_name, ".tex"),
+          include.rownames = FALSE)
   }
   return(tables)
 }
