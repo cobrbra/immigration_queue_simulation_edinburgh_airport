e2595a39e56ca56f5d375594c30f0cf74ec997ed
Jacob Bradley <cobrbradley@gmail.com>
2023-04-13 13:58:26 +0100

    feat: introduce complete_aircraft_arrivals function

--- code/process_data.R
+++ code/process_data.R
@@ -349,6 +349,10 @@ simulate_future_airport_classification <- function(aircrafts, quantile_list, see
   
 }
 
-
+complete_aircrafts_arrivals <- function(aircrafts_arrivals) {
+  
+  check_aircrafts_arrivals(complete_aircrafts_arrivals, complete = TRUE)
+  return(complete_aircrafts_arrivals)
+}
 
 
