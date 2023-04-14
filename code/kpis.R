e931aeb80e69c5395226d5a18773138178c3b152
Jacob Bradley <cobrbradley@gmail.com>
2023-04-14 18:43:50 +0100

    feat: simulate KPIs from passenger data

--- code/kpis.R
+++ code/kpis.R
@@ -1,89 +1,73 @@
-
-
-#### WAIT TIMES ####
-
-get_wait_times_raw <- function(passengers){
-  
-  wait_times <- passengers$bordercheck_start_time - passengers$route_datetime_int
-  
-  res <- data.frame(passenger_id = passengers$passenger_id, nationality = passengers$nationality, egate_eligibility = passengers$egate_eligibility,
-                    egate_used = passengers$egate_used, egate_failed = passengers$egate_failed, wait_times = wait_times)
-  
-  return(res)
-}
-
-
-
-waittimes_summary_helper <- function(waittimes){
-  
-  wt <- waittimes$wait_times
-  
-  res <- c(mean(wt), 
-           quantile(wt, c(0.5, 0.75, 0, 1)))
-  names(res)[1] <- "mean"
-  
-  return(res)
-}
-
-
-
-get_wait_times_summary <- function(passengers){
-  
-  waittimes <- get_wait_times_raw(passengers)
-  
-  res <- list(used_egate = waittimes_summary_helper(waittimes[passengers$egate_used == "egate", ]),
-              used_desk = waittimes_summary_helper(waittimes[passengers$egate_used == "desk", ]),
-              egate_failed = waittimes_summary_helper(waittimes[passengers$egate_failed == "failed", ]),
-              eligibility_egate = waittimes_summary_helper(waittimes[passengers$egate_eligibility == "eligible", ]), 
-              eligibility_noegate = waittimes_summary_helper(waittimes[passengers$egate_eligibility == "not_eligible", ]), 
-              coached = waittimes_summary_helper(waittimes[passengers$coached == TRUE, ]),
-              contact = waittimes_summary_helper(waittimes[passengers$coached == FALSE, ]), 
-              total = waittimes_summary_helper(waittimes))
-  
-  return(res)
-  
-  
-}
-
-
-
-
 ### QUEUE LENGTH ###
 
 
-queue_length_helper <- function(passengers, input_times){
-  
-  n_passengers <- dim(passengers)[1]
-  n_times <- length(input_times)
-  
-  times_matrix <- matrix(rep(input_times, each = n_passengers),
-                         nrow = n_passengers, byrow = FALSE) 
-  arrival_matrix <- matrix(rep(passengers$route_datetime_int, each = n_times),
-                           nrow = n_passengers, byrow = TRUE)
-  start_bordercheck_matrix <- matrix(rep(passengers$bordercheck_start_time, each = n_times),
-                                     nrow = n_passengers, byrow = TRUE)
-  
-  res_matrix <- arrival_matrix < times_matrix & start_bordercheck_matrix > times_matrix
-  
-  res <- colSums(res_matrix)
-  
-  return(res)  
+get_queue_length_at_time_point <- function(time_point, passengers) {
+  not_yet_done <- passengers$bordercheck_start_time > time_point
+  not_yet_started <- passengers$route_datetime_int < time_point
+  return(sum(not_yet_done & not_yet_started))
 }
 
-
-
-get_queue_length <- function(passengers, input_times){
-  
-  res <- data.frame(input_times,
-                    desk = queue_length_helper(passengers = passengers[passengers$egate_used == "desk", ], input_times = input_times),
-                    egate = queue_length_helper(passengers = passengers[passengers$egate_used == "egate", ], input_times = input_times),
-                    total = 0)
-  res$total <- res$desk + res$egate
-  
-  return(res)
+get_queue_lengths <- function(passengers, input_time_interval = 15*60){
+  queue_lengths <- data.frame(
+    queue_length_datetime_int = input_time_interval * unique(
+      round(passengers$route_datetime_int/input_time_interval)
+    )
+  ) %>% 
+    mutate(
+      desk_queue_length = map_int(
+        queue_length_datetime_int,
+        ~ get_queue_length_at_time_point(., filter(passengers, egate_used == "desk"))
+        ),
+      egate_queue_length = map_int(
+        queue_length_datetime_int,
+        ~ get_queue_length_at_time_point(., filter(passengers, egate_used == "egate"))
+        )
+      )
+  return(queue_lengths)
 }
 
-
+sim_queue_kpis <- function(passengers_after_routes,
+                           bordercheck_egates,
+                           egate_uptake_prop,
+                           wait_time_kpis,
+                           queue_length_kpis,
+                           egate_failure_prop,
+                           failed_egate_priority,
+                           seed,
+                           progress_bar = FALSE) {
+  
+  simulated_queues <- passengers_after_routes %>% 
+    nest(route_data = -sched_aircraft_date_posix) %>% 
+    mutate(year = format(sched_aircraft_date_posix, "%Y")) %>% 
+    mutate(queue_data = map(route_data, 
+                            ~ immigration_queue(., bordercheck_desks = bordercheck_desks, 
+                                                bordercheck_egates = bordercheck_egates, 
+                                                egate_uptake_prop = egate_uptake_prop, 
+                                                egate_failure_prop = egate_failure_prop, 
+                                                failed_egate_priority = failed_egate_priority, 
+                                                seed = seed),
+                            .progress = ifelse(progress_bar, "simulating queues", FALSE))) %>% 
+    select(-route_data) %>% 
+    unnest(queue_data) %>% 
+    nest(year_data = - year) %>% 
+    mutate(queue_lengths = map(year_data, get_queue_lengths, 
+                               .progress = ifelse(progress_bar, "getting queue lengths", FALSE)))
+  
+  for (kpi in wait_time_kpis) {
+    kpi_func = get(kpi)
+    simulated_queues[[kpi]] <- map_dbl(simulated_queues$year_data, kpi_func)
+  }
+  
+  for (kpi in queue_length_kpis) {
+    kpi_func = get(kpi)
+    simulated_queues[[kpi]]
+  }
+  
+  simulated_queue_kpis <- simulated_queues %>% 
+    select(-c(year_data, queue_lengths))
+  
+  return(simulated_queue_kpis)
+}
 
 mean_wait_time <- function(queue_data) {
   return(mean(queue_data$bordercheck_start_time - queue_data$route_datetime_int))
@@ -94,6 +78,55 @@ sla_wait_time <- function(queue_data, threshold_minutes = 60) {
 }
 
 
+# NOTE: I've muted out the wait times functions below,
+# because I'm changing the strategy of how we use KPIs.
+# I'm keeping them here commented as a reminder of what
+# Bella thought were useful metrics to track.
+
+# #### WAIT TIMES ####
+# 
+# get_wait_times_raw <- function(passengers){
+#   
+#   wait_times <- passengers$bordercheck_start_time - passengers$route_datetime_int
+#   
+#   res <- data.frame(passenger_id = passengers$passenger_id, nationality = passengers$nationality, egate_eligibility = passengers$egate_eligibility,
+#                     egate_used = passengers$egate_used, egate_failed = passengers$egate_failed, wait_times = wait_times)
+#   
+#   return(res)
+# }
+# 
+# 
+# 
+# waittimes_summary_helper <- function(waittimes){
+#   
+#   wt <- waittimes$wait_times
+#   
+#   res <- c(mean(wt), 
+#            quantile(wt, c(0.5, 0.75, 0, 1)))
+#   names(res)[1] <- "mean"
+#   
+#   return(res)
+# }
+# 
+# 
+# 
+# get_wait_times_summary <- function(passengers){
+#   
+#   waittimes <- get_wait_times_raw(passengers)
+#   
+#   res <- list(used_egate = waittimes_summary_helper(waittimes[passengers$egate_used == "egate", ]),
+#               used_desk = waittimes_summary_helper(waittimes[passengers$egate_used == "desk", ]),
+#               egate_failed = waittimes_summary_helper(waittimes[passengers$egate_failed == "failed", ]),
+#               eligibility_egate = waittimes_summary_helper(waittimes[passengers$egate_eligibility == "eligible", ]), 
+#               eligibility_noegate = waittimes_summary_helper(waittimes[passengers$egate_eligibility == "not_eligible", ]), 
+#               coached = waittimes_summary_helper(waittimes[passengers$coached == TRUE, ]),
+#               contact = waittimes_summary_helper(waittimes[passengers$coached == FALSE, ]), 
+#               total = waittimes_summary_helper(waittimes))
+#   
+#   return(res)
+#   
+#   
+# }
 
 
 
