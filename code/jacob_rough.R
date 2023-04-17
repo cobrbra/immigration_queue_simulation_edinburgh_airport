2a4a68814672e9315683c5e4bdb6f4262b94417b
Jacob Bradley <cobrbradley@gmail.com>
2023-04-17 01:06:41 +0100

    feat: some examples in jacob_rough

--- code/jacob_rough.R
+++ code/jacob_rough.R
@@ -14,6 +14,46 @@ tar_source(files = c(
   here("code/get_tables.R")
 ))
 
+# generate desks
+n_desks <- 9
+desk_means <- pmax(0, rep(90, n_desks))#rnorm(n_desks, mean = 90, sd = 5))
+desk_ids <- seq_len(n_desks)
+bordercheck_desks <- list(n_borderchecks = n_desks, 
+                          bordercheck_means = desk_means,
+                          bordercheck_sd = 14,
+                          bordercheck_ids = desk_ids)
+
+n_egates <- 20
+bordercheck_egates = list(
+  n_borderchecks = n_egates, 
+  bordercheck_mean = 45,
+  bordercheck_sd = 5,
+  bordercheck_ids = seq_len(n_egates))
+
+simulated_arrivals <- tar_read(future_aircrafts_arrivals) %>% 
+  complete_aircrafts_arrivals(tar_read(hubs), tar_read(countries), tar_read(prop_nationality), 
+                              tar_read(delay_dist), tar_read(n_passengers_quantiles), 
+                              tar_read(load_factor), tar_read(future_coached_levels)) %>% 
+  get_passengers_after_aircrafts() %>% 
+  get_passengers_after_routes(tar_read(coach_dist), tar_read(walk_dist), tar_read(base_walk_dist))
+
+simulated_queue <- simulated_arrivals %>% 
+  sim_queues(bordercheck_desks = bordercheck_desks, 
+             bordercheck_egates = bordercheck_egates, 
+             egate_uptake_prop = .9, elig_boost = 0.95, 
+             egate_failure_prop = tar_read(egate_failure_prop), 
+             failed_egate_priority = tar_read(failed_egate_priority),
+             seed = 1)
+
+simulated_queue %>% 
+  mutate(queue_time = bordercheck_start_time - route_datetime_int) %>% 
+  slice_sample(n = 2000) %>% 
+  # filter(year == "2023") %>% 
+  ggplot(aes(x = route_datetime_posix, y = queue_time / 3600, colour = egate_used)) +
+  geom_point() + 
+  ylim(0, NA) + 
+  facet_wrap(~year, nrow = 1, scales = "free_x")
+
 sim_settings <- generate_sim_settings()
 sim_results <- sim_analysis_data(sim_settings,
                                  tar_read(future_aircrafts_arrivals), 
