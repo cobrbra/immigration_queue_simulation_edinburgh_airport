d82e3f42325a522e6df4dbc912d72e0e61746d75
Jacob Bradley <cobrbradley@gmail.com>
2023-04-22 15:21:42 +0100

    fix: prototype rec fig

--- code/jacob_rough.R
+++ code/jacob_rough.R
@@ -44,7 +44,7 @@ simulated_arrivals <- tar_read(future_aircrafts_arrivals) %>%
 simulated_queue <- simulated_arrivals %>% 
   immigration_queue(bordercheck_desks = bordercheck_desks, 
              bordercheck_egates = bordercheck_egates, 
-             egate_uptake_prop = 1, target_eligibility = 1, 
+             egate_uptake_prop = .7, target_eligibility = .85, 
              egate_failure_prop = tar_read(egate_failure_prop), 
              failed_egate_priority = tar_read(failed_egate_priority),
              seed = 1)   
@@ -73,59 +73,96 @@ sim_results <- sim_analysis_data(sim_settings,
 
 # EXAMPLE MULTI-SIM EXPERIMENT SPECIFYING TRAJECTORY
 
-specify_sim_settings <- function(n_egates_range,
-                                 egate_uptake_range = seq(.7, .99, length.out = 5),
-                                 target_eligibility_range = seq(.85, .96, length.out = 5),
-                                 year_range = as.character(2023:2027),
-                                 n_gen_arrivals = 1,
-                                 n_gen_queues = 1,
-                                 seed = 1234) {
-  set.seed(seed)
-  gen_arrivals_seeds <- sample(1:100000, size = n_gen_arrivals)
-  gen_queue_seeds <- sample(1:100000, size = n_gen_queues)
-  
-  specificied_settings <- data.frame(
-    year = year_range,
-    n_egates = n_egates_range,
-    egate_uptake = egate_uptake_range,
-    target_eligibility = target_eligibility_range
-  )
-  
-  generated_settings <- crossing(
-    gen_arrivals_seed = gen_arrivals_seeds,
-    gen_queue_seed = gen_queue_seeds
-  )
-  
-  return(cross_join(specificied_settings, generated_settings))
+sim_settings <- specify_sim_settings(n_egates = c(10, 13, 15, 18, 21),
+                                    n_gen_arrivals = 5,
+                                    n_gen_queues = 5)
 
-}
+sim_results <- sim_analysis_data(sim_settings,
+                                 tar_read(future_aircrafts_arrivals), 
+                                 hubs = tar_read(hubs), 
+                                 countries = tar_read(countries), 
+                                 prop_nationality = tar_read(prop_nationality), 
+                                 delay_dist = tar_read(delay_dist), 
+                                 n_passengers_quantiles = tar_read(n_passengers_quantiles), 
+                                 egate_failure_prop = tar_read(egate_failure_prop),
+                                 failed_egate_priority = tar_read(failed_egate_priority),
+                                 coached_levels = tar_read(future_coached_levels), 
+                                 coach_dist = tar_read(coach_dist), 
+                                 walk_dist = tar_read(walk_dist), 
+                                 base_walk_dist = tar_read(base_walk_dist),
+                                 wait_time_kpis = c("mean_wait_time", "wait_time_60", "wait_time_15"),
+                                 save_data = FALSE)
 
-generate_sim_settings <- function(n_egates_range = seq(10, 14, by = 2),
-                                  egate_uptake_range = seq(.6, 1., by = .2),
-                                  target_eligibility_range = seq(.75, 1., .05),
-                                  year_range = as.character(2023:2027),
-                                  n_gen_arrivals = 1,
-                                  n_gen_queues = 1,
-                                  seed = 1234) {
-  set.seed(seed)
-  gen_arrivals_seeds <- sample(1:100000, size = n_gen_arrivals)
-  gen_queue_seeds <- sample(1:100000, size = n_gen_queues)
-  sim_settings <- crossing(gen_arrivals_seed = gen_arrivals_seeds, 
-                           gen_queue_seed = gen_queue_seeds, 
-                           n_egates = n_egates_range, 
-                           egate_uptake = egate_uptake_range, 
-                           target_eligibility = target_eligibility_range,
-                           year = year_range)
-  return(sim_settings)
-}
+core_recommendation_stats <- sim_results %>% 
+  summarise(wait_time_60_egate = mean(wait_time_60_egate), 
+            wait_time_60_desk = mean(wait_time_60_desk),
+            wait_time_15_desk = mean(wait_time_15_desk),
+            wait_time_15_egate = mean(wait_time_15_egate),
+            .by = c("year", "n_egates", "egate_uptake", "target_eligibility")) %>% 
+  pivot_longer(cols = c(wait_time_60_desk, wait_time_60_egate, wait_time_15_desk, wait_time_15_egate),
+               names_to = "which_kpi",
+               values_to = "kpi") %>%
+  mutate(overall_egate_usage = egate_uptake * target_eligibility,
+         check = if_else(str_detect(which_kpi, "desk"), "Desk", "eGate"),
+         which_kpi = if_else(str_detect(which_kpi, "15"), 
+                             "Proportion waits < 15mins", 
+                             "Proportion waits < 60mins"))
 
 
+panel_1 <- core_recommendation_stats %>%
+  distinct(year, egate_uptake, target_eligibility, overall_egate_usage, n_egates) %>% 
+  pivot_longer(c(egate_uptake, target_eligibility, overall_egate_usage, n_egates), 
+               names_to = "assumption", values_to = "value") %>%
+  mutate(
+    assumption = factor(
+      case_when(
+        assumption == "egate_uptake" ~ "eGate Uptake",
+        assumption == "overall_egate_usage" ~ "Overall eGate Usage",
+        assumption == "target_eligibility" ~ "eGate Eligibility",
+        assumption == "n_egates" ~ "n_egates"
+      ),
+      levels = c("n_egates", "eGate Eligibility", "eGate Uptake", "Overall eGate Usage")),
+    fac = factor(if_else(assumption == "n_egates", 
+                         "Recommended Number of eGates", 
+                         "Core Assumptions"),
+                 level = c("Recommended Number of eGates",
+                           "Core Assumptions"))) %>% 
+    ggplot(aes(x = year, y = value, fill = assumption)) + 
+    geom_col(position = "dodge", alpha = 0.9) + 
+    facet_wrap(~fac, ncol = 1, scales = "free_y") + 
+    theme_edi_airport() +
+    theme(legend.title = element_blank(),
+          legend.position = "bottom") + 
+    scale_fill_manual(breaks = c("eGate Eligibility", "eGate Uptake", "Overall eGate Usage"),
+                      values = c("Overall eGate Usage" = edi_airport_colours[4],
+                                 "eGate Uptake" = edi_airport_colours[5], 
+                                 "eGate Eligibility" = edi_airport_colours[6],
+                                 "n_egates" = edi_airport_colours[7])) + 
+    labs(x = "Year", y = "") +
+  guides(fill = guide_legend(nrow = 2, byrow = TRUE))
 
+panel_2 <- core_recommendation_stats %>% 
+  ggplot(aes(x = year, y = kpi, fill = check)) + 
+  geom_col(position = "dodge", alpha = 0.9) + 
+  facet_wrap(~which_kpi, ncol = 1) + 
+  labs(x = "Year", y = "") + 
+  theme_edi_airport() + 
+  theme(legend.title = element_blank(),
+        legend.position = "bottom") +
+  scale_fill_manual(values = edi_airport_colours[2:1]) +
+  guides(fill = guide_legend(nrow = 2, byrow = TRUE))
 
+prototype_rec_fig <- plot_grid(
+  (panel_1),
+  (panel_2)
+)
+prototype_rec_fig
 
-
-
-
+# SAVE MANUALLY
+ggsave(filename = here("figures/prototype_rec_fig.png"),
+       plot = prototype_rec_fig,
+       width = 12, 
+       height = 5)
 
 
 
