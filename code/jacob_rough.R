c3bac519207031ee1e01f080221d191ba2e538e0
Jacob Bradley <cobrbradley@gmail.com>
2023-05-12 23:04:40 +0100

    feat: experiments in jacob_rough

--- code/jacob_rough.R
+++ code/jacob_rough.R
@@ -34,10 +34,6 @@ bordercheck_egates = list(
   bordercheck_sd = 5)
 
 
-tar_make('prop_nationality')
-tar_read(prop_nationality)
-  
-
 simulated_arrivals <- tar_read(future_aircrafts_arrivals) %>% 
   complete_aircrafts_arrivals(tar_read(hubs), tar_read(countries), tar_read(prop_nationality), 
                               tar_read(delay_dist), tar_read(n_passengers_quantiles), 
@@ -53,8 +49,7 @@ simulated_queue <- simulated_arrivals %>%
              egate_failure_prop = tar_read(egate_failure_prop), 
              failed_egate_priority = tar_read(failed_egate_priority))   
 
-count(simulated_queue, egate_eligibility) %>% 
-  mutate(n = n / sum(n))
+
 
 # EXAMPLE MULTI-SIM EXPERIMENT
 
@@ -81,8 +76,8 @@ sim_results <- sim_analysis_data(sim_settings,
 # EXAMPLE MULTI-SIM EXPERIMENT SPECIFYING TRAJECTORY
 
 sim_settings <- specify_sim_settings(n_egates = c(10, 13, 16, 20, 25),
-                                    n_gen_arrivals = 10,
-                                    n_gen_queues = 10)
+                                    n_gen_arrivals = 2,
+                                    n_gen_queues = 2)
 
 sim_results <- sim_analysis_data(sim_settings,
                                  tar_read(future_aircrafts_arrivals), 
@@ -98,8 +93,11 @@ sim_results <- sim_analysis_data(sim_settings,
                                  walk_dist = tar_read(walk_dist), 
                                  base_walk_dist = tar_read(base_walk_dist),
                                  wait_time_kpis = c("mean_wait_time", "wait_time_60", "wait_time_15"),
-                                 queue_length_kpis = c("queue_length_650", "queue_length_1250"),
-                                 save_data = FALSE)
+                                 queue_length_kpis = c("exceeds_overflow", "exceeds_contingency"),
+                                 hall_desk_splits = seq(10,90,10),
+                                 save_data = TRUE)
+
+
 
 #### DELAY DIST FIG
 
@@ -147,15 +145,80 @@ bind_rows(
   scale_x_continuous(trans = scales::pseudo_log_trans(sigma = sigma),
                      breaks = round(pseudo_log_trans(sigma = sigma)$inverse(c(-3:-1, -0.25, 0, 0.25, 1:4))))#c(early_breaks, on_time_breaks, late_breaks))
 
-hall_split_desk = 0.6
-simulated_queue %>% 
-  get_queue_lengths() %>% 
-  mutate(year = lubridate::year(queue_length_date_posix)) %>% 
-  select(-c(queue_length_datetime_int, queue_length_date_posix)) %>% 
-  mutate(desk_queue_exceeds_hall = desk_queue_length >= 500*0.6) %>% 
-  mutate(egate_queue_exceeds_hall = egate_queue_length >= 500*0.4) %>% 
-  mutate(joint_queue_exceeds_overflow = desk_queue_length + egate_queue_length >= 650) %>% 
-  count(year, 
-        desk_queue_exceeds_hall&joint_queue_exceeds_overflow, 
-        egate_queue_exceeds_hall&joint_queue_exceeds_overflow)
 
+
+
+#### EXPERIMENTING TO SET RECOMMENDATIONS AND ASSUMPTIONS
+
+
+rec_fig_sim_settings <- specify_sim_settings(n_egates_range = c(15, 19, 21, 23, 23), 
+                                             egate_uptake_range = seq(.75, .99, length.out = 7)[2:6],
+                                             target_eligibility_range = seq(.8, .96, length.out = 7)[2:6],
+                                             n_gen_arrivals = 5,
+                                             n_gen_queues = 5)
+rec_fig_sim_data <- sim_analysis_data(
+                             rec_fig_sim_settings,
+                             tar_read(future_aircrafts_arrivals), 
+                             hubs = tar_read(hubs), 
+                             countries = tar_read(countries), 
+                             prop_nationality = tar_read(prop_nationality), 
+                             delay_dist = tar_read(delay_dist), 
+                             n_passengers_quantiles = tar_read(n_passengers_quantiles), 
+                             egate_failure_prop = tar_read(egate_failure_prop),
+                             failed_egate_priority = tar_read(failed_egate_priority),
+                             coached_levels = tar_read(future_coached_levels), 
+                             coach_dist = tar_read(coach_dist), 
+                             walk_dist = tar_read(walk_dist), 
+                             base_walk_dist = tar_read(base_walk_dist),
+                             wait_time_kpis = c("mean_wait_time", "wait_time_60", "wait_time_15", "wait_time_25"),
+                             queue_length_kpis = c("exceeds_overflow", "exceeds_contingency"),
+                             hall_desk_splits = seq(25, 75, 25),
+                             save_data = FALSE)
+
+rec_fig_sim_data %>% 
+  summarise(mean(wait_time_60_egate), .by = ("year"))
+
+rec_fig_sim_data %>% 
+  summarise(mean(wait_time_15_egate), .by = ("year"))
+
+rec_fig_sim_data %>% 
+  summarise(mean(exceeds_overflow_egate)/60, .by = ("year"))
+
+rec_fig_sim_data %>% 
+  summarise(mean(hall_split), .by = ("year"))
+
+minus_rec_fig_sim_settings <- specify_sim_settings(n_egates_range = c(10, 15, 19, 21, 23), 
+                                             egate_uptake_range = seq(.75, .99, length.out = 7)[2:6],
+                                             target_eligibility_range = seq(.8, .96, length.out = 7)[2:6],
+                                             n_gen_arrivals = 5,
+                                             n_gen_queues = 5)
+minus_rec_fig_sim_data <- sim_analysis_data(
+  minus_rec_fig_sim_settings,
+  tar_read(future_aircrafts_arrivals), 
+  hubs = tar_read(hubs), 
+  countries = tar_read(countries), 
+  prop_nationality = tar_read(prop_nationality), 
+  delay_dist = tar_read(delay_dist), 
+  n_passengers_quantiles = tar_read(n_passengers_quantiles), 
+  egate_failure_prop = tar_read(egate_failure_prop),
+  failed_egate_priority = tar_read(failed_egate_priority),
+  coached_levels = tar_read(future_coached_levels), 
+  coach_dist = tar_read(coach_dist), 
+  walk_dist = tar_read(walk_dist), 
+  base_walk_dist = tar_read(base_walk_dist),
+  wait_time_kpis = c("mean_wait_time", "wait_time_60", "wait_time_15", "wait_time_25"),
+  queue_length_kpis = c("exceeds_overflow", "exceeds_contingency"),
+  hall_desk_splits = seq(25, 75, 25),
+  save_data = FALSE)
+
+minus_rec_fig_sim_data %>% 
+  summarise(mean(wait_time_60_egate), .by = ("year"))
+
+minus_rec_fig_sim_data %>% 
+  summarise(mean(wait_time_15_egate), .by = ("year"))
+
+minus_rec_fig_sim_data %>% 
+  summarise(mean(exceeds_overflow_egate)/60, .by = ("year"))
+
+minus_rec_fig_sim_data %>% 
+  summarise(mean(hall_split), .by = ("year"))
